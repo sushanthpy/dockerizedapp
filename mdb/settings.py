58d8e0c7a30017025fdcf0310d4cb843c5d4ad9f
sushanth <sushanth53@gmail.com>
2017-08-14 12:34:53 -0400

    new changes

--- mdb/settings.py
+++ mdb/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'mobiles',
-    'django_jenkins'
+    #'django_jenkins'
 ]
 
 
@@ -93,7 +93,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-
+"""
 JENKINS_TASKS = (
     'django_jenkins.tasks.run_pylint',
 )
+"""
